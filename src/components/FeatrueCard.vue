304a36f2a3d50825f8ae2ac3fdee45c64b00a4de
xiaolangling <125276956+xiaolangling@users.noreply.github.com>
2023-02-16 01:01:07 +0800

    实现轮播组件
    写得很烂……恐怕需要再改，图片不知道从哪调用就先空着了

--- src/components/FeatrueCard.vue
+++ src/components/FeatrueCard.vue
@@ -1,9 +1,35 @@
 <template>
-  <div class="feac"></div>
+  <div class="feac">
+    <div class="feacPic">
+    <img src="./feacPic1" alt="图片1" id="pic1"/>
+    <img src="./feacPic2" alt="图片2" id="pic2"/>
+    <img src="./feacPic3" alt="图片3" id="pic3"/>
+    <img src="./feacPic4" alt="图片4" id="pic4"/>
+    <img src="./feacPic1" alt="图片1" id="pic5"/>
+  </div>
+  </div>
 </template>
-
-<style scoped>
-.feac {
+<style type="text/css">
+@keyframes feacMove {
+			0%,9% {transform: translateX(0px);}
+			16%,34% {transform: translateX(-600px);}
+			41%,59% {transform: translateX(-1200px);}
+			66%,84% {transform: translateX(-1800px);}
+			91%,100% {transform: translateX(-2400px);}
+		}
+  .feac{
+  width: 600px;
+  height: 629px;
   background: linear-gradient(#6de1ca, #54c5b9);
-}
+  overflow: hidden;
+  margin: 0 auto;
+		}
+		div.feacPic {
+			animation: feacMove 8s ease infinite normal;
+			width: 3000px;
+			transform:translateX(0px);
+		}
+		div.feacPic img {
+			float: left;
+		}
 </style>
